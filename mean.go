82394ef57c9e210da13f1647e6b778ec84e4b0bc
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-10-07 12:16:30 +0200

    Implement mean

---
+++ mean.go
@@ -0,0 +1,35 @@
+package arithmetic
+
+import (
+	"errors"
+	"fmt"
+)
+
+func init() {
+	RegisterFunction("mean", mean)
+}
+
+func mean(args ...interface{}) (interface{}, error) {
+
+	if len(args) == 0 {
+		return nil, errors.New("mean requires at least one argument")
+	}
+	
+	var sum float64
+	for _, a := range args {
+		switch t := a.(type) {
+		case float64:
+			sum += t
+		case variable:
+			v, ok := t.value.(float64)
+			if !ok {
+				return nil, fmt.Errorf("mean requires numeric arguments, %s given", t)
+			}
+			sum += v
+		default:
+			return nil, fmt.Errorf("mean requires numeric arguments, %v given", a)
+		}
+	}
+
+	return sum / float64(len(args)), nil
+}
