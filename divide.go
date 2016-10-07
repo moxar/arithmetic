e3b57b405e93a2c040b97388edadb05d508ae7b5
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-10-07 10:58:30 +0200

    Fix division by 0 error

--- divide.go
+++ divide.go
@@ -1,5 +1,9 @@
 package arithmetic
 
+import(
+	"errors"
+)
+
 type divide struct{}
 
 func (o divide) String() string {
@@ -19,6 +23,10 @@ func (o divide) solve(st *stack) (interface{}, error) {
 	left, err := st.popFloat()
 	if err != nil {
 		return nil, leftError(o, right)
+	}
+	
+	if right == 0 {
+		return nil, errors.New("division by 0")
 	}
 
 	return left / right, nil
