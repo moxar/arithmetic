03945cf1f77ff0dbe34dc110ae859921e1e93d2e
Alexandre Thomas <alexandre.thomas@closetome.fr>
2016-10-07 12:12:50 +0200

    Implement min

---
+++ min.go
@@ -0,0 +1,43 @@
+package arithmetic
+
+import (
+	"errors"
+	"fmt"
+)
+
+func init() {
+	RegisterFunction("min", min)
+}
+
+func min(args ...interface{}) (interface{}, error) {
+
+	var m float64
+	var def bool
+
+	var f float64
+	for _, a := range args {
+		switch t := a.(type) {
+		case float64:
+			f = t
+		case variable:
+			v, ok := t.value.(float64)
+			if !ok {
+				return nil, fmt.Errorf("min requires numeric arguments, %s given", t)
+			}
+			f = v
+		default:
+			return nil, fmt.Errorf("min requires numeric arguments, %v given", a)
+		}
+
+		if f < m || !def {
+			def = true
+			m = f
+		}
+	}
+
+	if !def {
+		return nil, errors.New("min requires at least one argument")
+	}
+
+	return m, nil
+}
